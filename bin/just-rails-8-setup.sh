55249e596b46dc2ebfec613149e1e405a3265afe
Michael Kyed <mkyed@relatel.dk>
2025-04-26 10:00:14 +0200

    save

--- bin/just-rails-8-setup.sh
+++ bin/just-rails-8-setup.sh
@@ -4,6 +4,7 @@ if [ ! -d app ]
 then
   rails new . --database=sqlite3 --force
   bundle exec rails db:setup 
+  bundle exec rails db:migrate db:test:prepare
 fi
 
 bundle exec rails server -b 0.0.0.0
