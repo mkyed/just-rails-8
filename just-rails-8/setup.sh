54eff8d9ba774abe75243362209ff139f4b0114a
Michael Kyed <mkyed@relatel.dk>
2025-04-28 21:10:48 +0200

    maglev boots

--- just-rails-8/setup.sh
+++ just-rails-8/setup.sh
@@ -6,17 +6,27 @@
 if [ ! -d app ]
 then
   curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
-  apt-get update
   apt install -y nodejs
   npm install -g yarn
 
-  node --version
-  npm --version
-  yarn --version
-
   gem install just-rails-8/maglevcms-2.0.0.gem
-  rails new . -m https://raw.githubusercontent.com/maglevhq/maglev-core/master/template.rb \
-    --database=sqlite3 --skip-action-cable
+
+  rails new . --database=sqlite3 --skip-action-cable
+
+  bundle add maglevcms -v '~> 2.0.0'
+  bundle add maglevcms-hyperui-kit -v '~> 1.2.0'
+  bundle add image_processing
+  bundle install
+
+  grep cms Gemfile
+  grep cms Gemfile.lock
+
+  bundle exec rails db:create
+  bundle exec rails active_storage:install
+  bundle exec rails db:migrate
+  bundle exec rails g maglev:install
+  bundle exec rails g maglev:hyperui:install --force
+  bundle exec rails maglev:create_site
 fi
 
 bundle exec rails server -b 0.0.0.0
