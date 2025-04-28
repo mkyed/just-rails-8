82923f66211b1767f5bf659e9d004c45fb4eab29
Michael Kyed <mkyed@relatel.dk>
2025-04-28 20:15:32 +0200

    saving

--- just-rails-8/setup.sh
+++ just-rails-8/setup.sh
@@ -5,6 +5,16 @@
 
 if [ ! -d app ]
 then
+  curl -fsSL https://deb.nodesource.com/setup_20.x | bash -
+  apt-get update
+  apt install -y nodejs
+  npm install -g yarn
+
+  node --version
+  npm --version
+  yarn --version
+
+  gem install just-rails-8/maglevcms-2.0.0.gem
   rails new . -m https://raw.githubusercontent.com/maglevhq/maglev-core/master/template.rb \
     --database=sqlite3 --skip-action-cable
 fi
